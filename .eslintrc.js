11b510d61f850bcbb3754ab3d5abbe2377e8abfa
Franklin <fuhb_0309@163.com>
2023-05-14 18:06:24 +0800

    完善eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,22 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // 指定解析器
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  globals: {},
+  plugins: ["@typescript-eslint", "react", "prettier"],
   extends: [
+    "eslint:recommended",
     "plugin:react/recommended", // @eslint-plugin-react 的推荐规则
     "plugin:@typescript-eslint/recommended", // @typescript-eslint/eslint-plugin的推荐规则
-    "plugin:prettier/recommended", // 使用eslint-config-prettier 来禁用eslint插件的格式化规则
+    "plugin:import/typescript", // eslint-plugin-import 抛出导入等支持的规则
+    "plugin:react-hooks/recommended", // eslint-plugin-react-hooks 的规则
+    "plugin:prettier/recommended", // eslint-plugin-prettier 的推荐规则
   ],
+  parser: "@typescript-eslint/parser", // 指定解析器
   parserOptions: {
-    ecmaVersion: 2020, // 允许解析最新的特性
+    ecmaVersion: 6, // 允许解析那个版本的特性
     sourceType: "module", // 允许使用 import
     ecmaFeatures: {
       jsx: true, // 允许对JSX进行解析
